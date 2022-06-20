663e8757a6eb1d6b43ce21ca596414ca9aa6c2e6
obendesmond <obendesmond2@gmail.com>
2022-06-20 18:54:46 +0100

    finish the login screen

--- App.js
+++ App.js
@@ -1,39 +1,6 @@
 import { useState } from "react";
-import { TextInput, Text, Switch } from "react-native";
-import Screen from "./app/components/Screen";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  {
-    label: "Furniture",
-    value: 1,
-  },
-  {
-    label: "Clothing",
-    value: 2,
-  },
-  {
-    label: "Cameras",
-    value: 3,
-  },
-  {
-    label: "Device",
-    value: 4,
-  },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState("");
-  return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelect={item => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-    </Screen>
-  );
+  return <LoginScreen />;
 }
